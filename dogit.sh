1675a88aff4934e18d33e61972e52a68d89caac2
AE <aryan.eftekhari@gmail.com>
2014-07-15 22:41:32 -0700

    Initial Commit

---
+++ dogit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+init_repo () {
+
+  if [ -z "$1" ]; then
+    GITDIR="/Users/aryan-poinat/Google-Drive/_git"
+  else
+    GITDIR=$1
+  fi
+
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  REP=$(basename $DIR)
+  echo "Creating Repository:" $GITDIR/$REP
+
+  cd $GITDIR
+  sudo git init --bare $REP.git
+
+  cd $DIR
+  sudo git init
+  sudo touch README.md
+  sudo git add .
+  sudo git commit README.md -m "Dogit commit."
+  sudo git remote add origin file://$GITDIR/$REP.git
+  sudo git push origin master
+}
+
+while true; do
+    read -p "You Google Drive Git Directory (Default: ~/Google-Drive/_git) ?" L
+    init_repo $L
+    break
+done
